efd72e93246e934bce1fa625fe1a9a59f0552df8
Martin Orell <martin0stars@gmail.com>
2026-07-24 10:04:09 +0200

    prettify ToolContainer

--- src/Tool/ToolContainer.cpp
+++ src/Tool/ToolContainer.cpp
@@ -6,36 +6,42 @@
 
 bool sigrid::ToolContainer::load(std::istream& is){
 
-    auto string_o = sigrid_config::readString(is);
-    if(string_o == std::nullopt){
-        return false;
+    {
+        const auto string_o = sigrid_config::readString(is);
+        if(string_o == std::nullopt){
+            return false;
+        }
+        const std::string& s = string_o.value();
+
+        if(s != "["){
+            return false;
+        }
     }
-    std::string s = string_o.value();
-    if(s == "["){
-        while(string_o = sigrid_config::readString(is)){
-            
-            if(string_o == std::nullopt){
+
+    while(const auto string_o = sigrid_config::readString(is)){
+        
+        if(string_o == std::nullopt){
+            return false;
+        }
+        const std::string& s = string_o.value();
+
+        if(s == "]"){
+            break;
+        }
+        else if(s == "selection:"){
+            auto selection_o = sigrid_config::readString(is);
+            if(selection_o == std::nullopt){
                 return false;
             }
-            s = string_o.value();
-            if(s == "]"){
-                break;
-            }
-            else if(s == "selection:"){
-                auto selection_o = sigrid_config::readString(is);
-                if(selection_o == std::nullopt){
-                    return false;
-                }
-                selection = selection_o.value();
-            }
-            else if(s == "color:"){
-                is >> colorId;
-            }
-            else{
-                std::cerr << "ToolContainer: Unknown key: \"" << s << "\"";
-                std::cerr << ". Failed to load toolContainer" << std::endl;
-                return false;
-            }
+            selection = selection_o.value();
+        }
+        else if(s == "color:"){
+            is >> colorId;
+        }
+        else{
+            std::cerr << "ToolContainer: Unknown key: \"" << s << "\"";
+            std::cerr << ". Failed to load toolContainer" << std::endl;
+            return false;
         }
     }
     return true;
