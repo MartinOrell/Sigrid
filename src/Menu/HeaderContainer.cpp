f25e03bbeabba632c7b1d079c67d7639c3e8f3bf
Martin Orell <martin0stars@gmail.com>
2026-07-28 04:34:05 +0200

    use loadNamedContainers in HeaderContainer to load items

--- src/Menu/HeaderContainer.cpp
+++ src/Menu/HeaderContainer.cpp
@@ -1,30 +1,7 @@
 #include "Menu/HeaderContainer.h"
 
-#include "Config/IO.h"
+#include "Config/LoadNamedContainers.h"
 
 bool sigrid::HeaderContainer::load(std::istream& is){
-
-    if(sigrid_config::readString(is) != "["){
-        return false;
-    }
-
-    while(const auto string_o = sigrid_config::readString(is)){
-        
-        if(string_o == std::nullopt){
-            return false;
-        }
-        const std::string& s = string_o.value();
-
-        if(s == "]"){
-            break;
-        }
-
-        MenuItemContainer item;
-        item.name = s;
-        if(!item.load(is)){
-            return false;
-        }
-        items.push_back(std::move(item));
-    }
-    return true;
+    return sigrid_config::loadNamedContainers<MenuItemContainer>(items, is);
 }
