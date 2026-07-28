fb2c097a84f79accb688eb3bfd4ae24ee9b97021
Martin Orell <martin0stars@gmail.com>
2026-07-28 08:39:01 +0200

    Added error messages when failed to load MainConfigContainer

--- src/Config/MainConfigContainer.cpp
+++ src/Config/MainConfigContainer.cpp
@@ -15,6 +15,8 @@ bool MainConfigContainer::load(const std::string& filename){
     std::ifstream ifs(filename);
 
     if(!ifs.is_open()){
+        std::cerr << "MainConfigContainer: Failed opening " << filename
+            << " loading failed" << std::endl;
         return false;
     }
 
@@ -22,53 +24,113 @@ bool MainConfigContainer::load(const std::string& filename){
 
         const auto key_o = sigrid_config::readString(ifs);
         if(key_o == std::nullopt){
+
+            std::cerr << "MainConfigContainer: Failed reading string."
+                << " loading failed" << std::endl;
             return false;
         }
         const std::string& key = key_o.value();
         
         if(key == "Window:"){
-            mainWindow.load(ifs);
+
+            if(!mainWindow.load(ifs)){
+
+                std::cerr << "MainConfigContainer: Failed loading Window."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "TileColors:"){
 
             if(!loadValueContainers<sigrid::ColorContainer>(tileColors, ifs)){
+                std::cerr << "MainConfigContainer: Failed loading tileColors."
+                    << " loading failed" << std::endl;
                 return false;
             }
         }
         else if(key == "ArrowColors:"){
 
             if(!loadValueContainers<sigrid::ColorContainer>(arrowColors, ifs)){
+                std::cerr << "MainConfigContainer: Failed loading arrowColors."
+                    << " loading failed" << std::endl;
                 return false;
             }
         }
         else if(key == "PieceColors:"){
-            loadContainers<sigrid::PieceColor>(pieceColors, ifs);
+
+            if(!loadContainers<sigrid::PieceColor>(pieceColors, ifs)){
+                std::cerr << "MainConfigContainer: Failed loading pieceColors."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "Pieces:"){
-            loadContainers<sigrid::PieceContainer>(pieces, ifs);
+
+            if(!loadContainers<sigrid::PieceContainer>(pieces, ifs)){
+                std::cerr << "MainConfigContainer: Failed loading pieces."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "BoardStyle:"){
-            boardData.load(ifs);
+
+            if(!boardData.load(ifs)){
+                std::cerr << "MainConfigContainer: Failed loading boardStyle."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "Menu:"){
-            menuData.load(ifs);
+
+            if(!menuData.load(ifs)){
+                std::cerr << "MainConfigContainer: Failed loading Menu."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "ToolPicker:"){
-            toolPickerData.load(ifs);
+
+            if(!toolPickerData.load(ifs)){
+
+                std::cerr << "MainConfigContainer: Failed loading toolPicker."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "LeftClickTool:"){
-            leftClickTool.load(ifs);
+
+            if(!leftClickTool.load(ifs)){
+
+                std::cerr << "MainConfigContainer: Failed loading leftClickTool."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "RightClickTool:"){
-            rightClickTool.load(ifs);
+
+            if(!rightClickTool.load(ifs)){
+
+                std::cerr << "MainConfigContainer: Failed loading rightClickTool."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "MiddleClickTool:"){
-            middleClickTool.load(ifs);
+
+            if(!middleClickTool.load(ifs)){
+
+                std::cerr << "MainConfigContainer: Failed loading middleClickTool."
+                    << " loading failed" << std::endl;
+                return false;
+            }
         }
         else if(key == "resetFilename:"){
 
             const auto resetBoardFilename_o = readString(ifs);
             if(resetBoardFilename_o == std::nullopt){
+
+                std::cerr << "MainConfigContainer: Failed reading string for resetBoardFilename."
+                    << " loading failed" << std::endl;
                 return false;
             }
             resetBoardFilename = resetBoardFilename_o.value();
@@ -77,6 +139,9 @@ bool MainConfigContainer::load(const std::string& filename){
 
             const auto defaultBoardImageFilename_o = readString(ifs);
             if(defaultBoardImageFilename_o == std::nullopt){
+
+                std::cerr << "MainConfigContainer: Failed reading string for defaultBoardImageFilename."
+                    << " loading failed" << std::endl;
                 return false;
             }
             defaultBoardImageFilename = defaultBoardImageFilename_o.value();
@@ -85,13 +150,16 @@ bool MainConfigContainer::load(const std::string& filename){
 
             const auto boardFilename_o = readString(ifs);
             if(boardFilename_o == std::nullopt){
+
+                std::cerr << "MainConfigContainer: Failed reading string for boardFilename"
+                    << " loading failed" << std::endl;
                 return false;
             }
             boardFilename = boardFilename_o.value();
         }
         else{
             std::cerr << "MainConfigContainer: Unknown key: \"" << key << "\"" << std::endl;
-            std::cerr << "read from startup file: \"" << filename << "\"" << std::endl;
+            std::cerr << " read from startup file: \"" << filename << "\"" << std::endl;
             return false;
         }
     }
