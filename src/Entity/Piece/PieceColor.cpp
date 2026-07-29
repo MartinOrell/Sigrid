36c0ef9661ed55f0bdec2e8cdc0000683796fadc
Martin Orell <martin0stars@gmail.com>
2026-07-29 07:54:43 +0200

    Add error messages when fail to load PieceColor

--- src/Entity/Piece/PieceColor.cpp
+++ src/Entity/Piece/PieceColor.cpp
@@ -10,40 +10,64 @@ bool sigrid::PieceColor::load(std::istream& is){
     while(auto string_o = sigrid_config::readString(is)){
         
         if(string_o == std::nullopt){
+
+            std::cerr << "PieceColor: Failed to read string."
+                << " Failed to load PieceColor" << std::endl; 
             return false;
         }
         std::string s = string_o.value();
+
         if(s == "]"){
             break;
         }
         else if(s == "name:"){
+
             auto name_o = sigrid_config::readString(is);
             if(name_o == std::nullopt){
+
+                std::cerr << "PieceColor: Failed to read name."
+                    << " Failed to load PieceColor" << std::endl;
                 return false;
             }
             std::string name = name_o.value();
         }
         else if(s == "style:"){
+
             auto styleString_o = sigrid_config::readString(is);
             if(styleString_o == std::nullopt){
+
+                std::cerr << "PieceColor: Failed to read style."
+                    << " Failed to load PieceColor" << std::endl;
                 return false;
             }
             std::string style = styleString_o.value();
             isLight = style == "light";
         }
         else if(s == "lightModifier:"){
+
             ColorContainer color;
-            color.load(is);
+            if(!color.load(is)){
+
+                std::cerr << "PieceColor: Failed to load lightModifier."
+                    << " Failed to load PieceColor" << std::endl;
+                return false;
+            }
             lightModifier = color.value;
         }
         else if(s == "darkModifier:"){
+
             ColorContainer color;
-            color.load(is);
+            if(!color.load(is)){
+
+                std::cerr << "PieceColor: Failed to load darkModifier."
+                    << " Failed to load PieceColor" << std::endl;
+                return false;
+            }
             darkModifier = color.value;
         }
         else{
-            std::cerr << "PieceColor: Unknown key: \"" << s << "\"";
-            std::cerr << ". Failed to load PieceColor" << std::endl;
+            std::cerr << "PieceColor: Unknown key: \"" << s << "\".";
+            std::cerr << " Failed to load PieceColor" << std::endl;
             return false;
         }
     }
