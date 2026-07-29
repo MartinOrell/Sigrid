dc02ca71f04898a9a4a81073f8f8316187790e02
Martin Orell <martin0stars@gmail.com>
2026-07-29 08:11:30 +0200

    Add error messages when fail to load BoardDesignContainer

--- src/Board/BoardDesignContainer.cpp
+++ src/Board/BoardDesignContainer.cpp
@@ -8,12 +8,18 @@
 bool sigrid::BoardDesignContainer::load(std::istream& is){
 
     if(sigrid_config::readString(is) != "["){
+
+        std::cerr << "BoardDesignContainer: Failed to read initial \"[\"."
+            << " Failed to load BoardDesignContainer" << std::endl;
         return false;
     }
 
     while(const auto string_o = sigrid_config::readString(is)){
         
         if(string_o == std::nullopt){
+
+            std::cerr << "BoardDesignContainer: Failed to read string."
+                << " Failed to load BoardDesignContainer" << std::endl;
             return false;
         }
         const std::string& s = string_o.value();
@@ -24,42 +30,60 @@ bool sigrid::BoardDesignContainer::load(std::istream& is){
         else if(s == "Tile:"){
 
             if(!tile.load(is)){
+
+                std::cerr << "BoardDesignContainer: Failed to load Tile."
+                    << " Failed to load BoardDesignContainer" << std::endl;
                 return false;
             }
         }
         else if(s == "Arrow:"){
 
             if(!arrow.load(is)){
+
+                std::cerr << "BoardDesignContainer: Failed to load Arrow."
+                    << " Failed to load BoardDesignContainer" << std::endl;
                 return false;
             }
         }
         else if(s == "Circle:"){
 
             if(!circle.load(is)){
+
+                std::cerr << "BoardDesignContainer: Failed to load Circle."
+                    << " Failed to load BoardDesignContainer" << std::endl;
                 return false;
             }
         }
         else if(s == "CoordLabels:"){
 
             if(!sigrid_config::loadContainers<BoardLabelContainer>(labels, is)){
+
+                std::cerr << "BoardDesignContainer: Failed to load CoorcLabels."
+                    << " Failed to load BoardDesignContainer" << std::endl;
                 return false;
             }
         }
         else if(s == "Border:"){
 
             if(!border.load(is)){
+
+                std::cerr << "BoardDesignContainer: Failed to load Border."
+                    << " Failed to load BoardDesignContainer" << std::endl;
                 return false;
             }
         }
         else if(s == "TurnToken:"){
 
             if(!turnToken.load(is)){
+
+                std::cerr << "BoardDesignContainer: Failed to load TurnToken."
+                    << " Failed to load BoardDesignContainer" << std::endl;
                 return false;
             }
         }
         else{
-            std::cerr << "BoardDesignContainer: Unknown key: \"" << s << "\"";
-            std::cerr << ". Failed to load BoardDesignContainer" << std::endl;
+            std::cerr << "BoardDesignContainer: Unknown key: \"" << s << "\".";
+            std::cerr << " Failed to load BoardDesignContainer" << std::endl;
             return false;
         }
     }
