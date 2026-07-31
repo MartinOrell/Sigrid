6ec9cf956cad656a87f5e2978a8a7ecb94778d7d
Martin Orell <martin0stars@gmail.com>
2026-07-31 07:33:15 +0200

    Added error messages when failed to load TurnTokenContainer

--- src/Entity/TurnToken/TurnTokenContainer.cpp
+++ src/Entity/TurnToken/TurnTokenContainer.cpp
@@ -7,12 +7,18 @@
 bool sigrid::TurnTokenContainer::load(std::istream& is){
 
     if(sigrid_config::readString(is) != "["){
+
+        std::cerr << "TurnTokenContainer: Failed to read initial \"[\"."
+            << " Failed to load TurnTokenContainer" << std::endl;
         return false;
     }
 
     while(const auto string_o = sigrid_config::readString(is)){
         
         if(string_o == std::nullopt){
+
+            std::cerr << "TurnTokenContainer: Failed to read string."
+                << " Failed to load TurnTokenContainer" << std::endl;
             return false;
         }
         const std::string& s = string_o.value();
@@ -24,13 +30,16 @@ bool sigrid::TurnTokenContainer::load(std::istream& is){
 
             const auto isVisible_o = sigrid_config::readVisibility(is);
             if(isVisible_o == std::nullopt){
+
+                std::cerr << "TurnTokenContainer: Failed to read visibility."
+                    << " Failed to load TurnTokenContainer" << std::endl;
                 return false;
             }
             isVisible = isVisible_o.value();
         }
         else{
-            std::cerr << "TurnTokenContainer: Unknown key: \"" << s << "\"";
-            std::cerr << ". Failed to load turn token" << std::endl;
+            std::cerr << "TurnTokenContainer: Unknown key: \"" << s << "\".";
+            std::cerr << " Failed to load TurnTokenContainer" << std::endl;
             return false;
         }
     }
