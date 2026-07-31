4f60403049109036ea628e1caa2f549a98d47063
Martin Orell <martin0stars@gmail.com>
2026-07-31 08:34:39 +0200

    Added error messages when failed to load ToolContainer

--- src/Tool/ToolContainer.cpp
+++ src/Tool/ToolContainer.cpp
@@ -7,12 +7,18 @@
 bool sigrid::ToolContainer::load(std::istream& is){
 
     if(sigrid_config::readString(is) != "["){
+
+        std::cerr << "ToolContainer: Failed to read initial \"[\"."
+            << " Failed to load ToolContainer" << std::endl;
         return false;
     }
 
     while(const auto string_o = sigrid_config::readString(is)){
         
         if(string_o == std::nullopt){
+
+            std::cerr << "ToolContainer: Failed to read string."
+                << " Failed to load ToolContainer" << std::endl;
             return false;
         }
         const std::string& s = string_o.value();
@@ -24,6 +30,9 @@ bool sigrid::ToolContainer::load(std::istream& is){
 
             const auto selection_o = sigrid_config::readString(is);
             if(selection_o == std::nullopt){
+
+                std::cerr << "ToolContainer: Failed to read string for selection."
+                    << " Failed to load ToolContainer" << std::endl;
                 return false;
             }
             selection = selection_o.value();
@@ -32,13 +41,16 @@ bool sigrid::ToolContainer::load(std::istream& is){
 
             const auto colorId_o = sigrid_config::readInt(is);
             if(colorId_o == std::nullopt){
+
+                std::cerr << "ToolContainer: Failed read int for color."
+                    << " Failed to load ToolContainer" << std::endl;
                 return false;
             }
             colorId = colorId_o.value();
         }
         else{
-            std::cerr << "ToolContainer: Unknown key: \"" << s << "\"";
-            std::cerr << ". Failed to load toolContainer" << std::endl;
+            std::cerr << "ToolContainer: Unknown key: \"" << s << "\".";
+            std::cerr << " Failed to load toolContainer" << std::endl;
             return false;
         }
     }
