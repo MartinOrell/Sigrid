48448fcc2913c2ad4224ccfb5f25d7fa601aa26c
Martin Orell <martin0stars@gmail.com>
2026-07-31 07:46:15 +0200

    Added error message when failed to load HeaderContainer

--- src/Menu/HeaderContainer.cpp
+++ src/Menu/HeaderContainer.cpp
@@ -3,5 +3,12 @@
 #include "Config/LoadNamedContainers.h"
 
 bool sigrid::HeaderContainer::load(std::istream& is){
-    return sigrid_config::loadNamedContainers<MenuItemContainer>(items, is);
+
+    if(!sigrid_config::loadNamedContainers<MenuItemContainer>(items, is)){
+
+        std::cerr << "HeaderContainer: Failed to load items."
+            << " Failed to load HeaderContainer" << std::endl;
+        return false;
+    }
+    return true;
 }
