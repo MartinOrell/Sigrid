0f9bc63dacbdee25397654c6bde0656642a988cf
Martin Orell <martin0stars@gmail.com>
2026-08-25 16:13:07 +0200

    remove using namespace in InputHandler.cpp

--- src/sigrid/Input/InputHandler.cpp
+++ src/sigrid/Input/InputHandler.cpp
@@ -1,24 +1,22 @@
 #include "sigrid/Input/InputHandler.h"
 
-using namespace sigrid;
-
-void InputHandler::addTool(const sf::Mouse::Button& button, sigrid::Tool tool){
+void sigrid::InputHandler::addTool(const sf::Mouse::Button& button, sigrid::Tool tool){
     m_tools.insert(button,std::move(tool));
 }
 
-void InputHandler::addTool(const sf::Keyboard::Key& button, sigrid_action::Action action){
+void sigrid::InputHandler::addTool(const sf::Keyboard::Key& button, sigrid_action::Action action){
     m_keyboardActions.insert(button, std::move(action));
 }
 
-void InputHandler::addCtrlTool(const sf::Keyboard::Key& button, sigrid_action::Action action){
+void sigrid::InputHandler::addCtrlTool(const sf::Keyboard::Key& button, sigrid_action::Action action){
     m_ctrlKeyboardActions.insert(button, std::move(action));
 }
 
-void InputHandler::addCtrlShiftTool(const sf::Keyboard::Key& button, sigrid_action::Action action){
+void sigrid::InputHandler::addCtrlShiftTool(const sf::Keyboard::Key& button, sigrid_action::Action action){
     m_ctrlShiftKeyboardActions.insert(button, std::move(action));
 }
 
-void InputHandler::setEntity(const sf::Mouse::Button& button, const sigrid::LogicEntity& logicEntity){
+void sigrid::InputHandler::setEntity(const sf::Mouse::Button& button, const sigrid::LogicEntity& logicEntity){
 
     auto tool_o = m_tools.at(button);
     if(tool_o == std::nullopt){
@@ -29,7 +27,7 @@ void InputHandler::setEntity(const sf::Mouse::Button& button, const sigrid::Logi
     tool.setEntity(logicEntity);
 }
 
-void InputHandler::setArrow(const sf::Mouse::Button& button, const int& colorId){
+void sigrid::InputHandler::setArrow(const sf::Mouse::Button& button, const int& colorId){
 
     auto tool_o = m_tools.at(button);
     if(tool_o == std::nullopt){
@@ -40,7 +38,7 @@ void InputHandler::setArrow(const sf::Mouse::Button& button, const int& colorId)
     tool.setArrow(colorId);
 }
 
-void InputHandler::setSelection(const sf::Mouse::Button& button, const ToolSelection& selection){
+void sigrid::InputHandler::setSelection(const sf::Mouse::Button& button, const ToolSelection& selection){
     
     auto tool_o = m_tools.at(button);
     if(tool_o == std::nullopt){
@@ -51,7 +49,7 @@ void InputHandler::setSelection(const sf::Mouse::Button& button, const ToolSelec
     tool.setSelection(selection);
 }
 
-std::optional<sigrid::Tool*> InputHandler::getToolPtr(const sf::Mouse::Button& button){
+std::optional<sigrid::Tool*> sigrid::InputHandler::getToolPtr(const sf::Mouse::Button& button){
 
     auto tool_o = m_tools.at(button);
     if(tool_o == std::nullopt){
@@ -60,13 +58,14 @@ std::optional<sigrid::Tool*> InputHandler::getToolPtr(const sf::Mouse::Button& b
     return &(tool_o.value().get());
 }
 
-std::optional<sigrid_action::Action> InputHandler::getAction(const sf::Keyboard::Key& key){
+std::optional<sigrid_action::Action> sigrid::InputHandler::getAction(const sf::Keyboard::Key& key){
     return m_keyboardActions.at(key);
 }
 
-std::optional<sigrid_action::Action> InputHandler::getCtrlAction(const sf::Keyboard::Key& key){
+std::optional<sigrid_action::Action> sigrid::InputHandler::getCtrlAction(const sf::Keyboard::Key& key){
     return m_ctrlKeyboardActions.at(key);
 }
 
-std::optional<sigrid_action::Action> InputHandler::getCtrlShiftAction(const sf::Keyboard::Key& key){
-    return m_ctrlShiftKeyboardActions.at(key);}
+std::optional<sigrid_action::Action> sigrid::InputHandler::getCtrlShiftAction(const sf::Keyboard::Key& key){
+    return m_ctrlShiftKeyboardActions.at(key);
+}
